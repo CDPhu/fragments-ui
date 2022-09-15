47b6617733102c65a746eb74bdef72d860cfe32d
LostButton <71197188+LostButton@users.noreply.github.com>
2022-09-15 16:53:30 -0400

    functional api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 // src/js/api.js
 
 // fragments microservice API, defaults to localhost:8080
-const apiUrl = process.env.API_URL || 'http://localhost:1234';
+const apiUrl = process.env.API_URL || 'http://localhost:8080';
 
 /**
  * Given an authenticated user, request all fragments for this user from the
